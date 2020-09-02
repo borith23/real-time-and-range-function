578fd2879b90fc3d2e0853ef9c578b3dab5b4906
Borith MOEK <53550645+borith23@users.noreply.github.com>
2020-09-02 17:13:05 +0700

    range python

--- index.py
+++ index.py
@@ -1,24 +1,37 @@
-import datetime
+# import datetime
 
-x = datetime.datetime.now()
+# x = datetime.datetime.now()
 
-print(x.strftime("%d") + "/" + x.strftime("%A") + "/" + x.strftime("%B") + "/" + x.strftime("%Y"))
+# print(x.strftime("%d") + "/" + x.strftime("%A") + "/" + x.strftime("%B") + "/" + x.strftime("%Y"))
 
-print(x.strftime("%X") + " " + x.strftime("%p"))
+# print(x.strftime("%X") + " " + x.strftime("%p"))
 
-print(x.strftime("%c"))
+# print(x.strftime("%c"))
 
-realDateTime = datetime.datetime.now()
-print(realDateTime.strftime("%c"))
+# realDateTime = datetime.datetime.now()
+# print(realDateTime.strftime("%c"))
 
-for i in "Datacamp":
-    if i == 'a':
-        print (i)
+# for i in "Datacamp":
+#     if i == 'a':
+#         print (i)
 
-print("-----------------")
+# print("-----------------")
 
-for i in range(10):
-    for j in range(i):
-        print (i, end=' ')
-    print()
+# for i in range(10):
+#     for j in range(i):
+#         print (i, end=' ')
+#     print()
 
+
+
+
+# Python program to concatenate 
+# the result of two range functions 
+from itertools import chain 
+   
+# Using chain method 
+print("Concatenating the result") 
+res = chain(range(5), range(10, 20, 2)) 
+  
+for i in res: 
+    print(i, end=" ") 
