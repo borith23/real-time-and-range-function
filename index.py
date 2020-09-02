09d8ca0d02bad63a06c994724d5e306ce352d966
Borith MOEK <53550645+borith23@users.noreply.github.com>
2020-09-02 17:24:14 +0700

    open code

--- index.py
+++ index.py
@@ -1,29 +1,29 @@
-# import datetime
+import datetime
 
-# x = datetime.datetime.now()
+x = datetime.datetime.now()
 
-# print(x.strftime("%d") + "/" + x.strftime("%A") + "/" + x.strftime("%B") + "/" + x.strftime("%Y"))
+print(x.strftime("%d") + "/" + x.strftime("%A") + "/" + x.strftime("%B") + "/" + x.strftime("%Y"))
 
-# print(x.strftime("%X") + " " + x.strftime("%p"))
+print(x.strftime("%X") + " " + x.strftime("%p"))
 
-# print(x.strftime("%c"))
+print(x.strftime("%c"))
 
-# realDateTime = datetime.datetime.now()
-# print(realDateTime.strftime("%c"))
+realDateTime = datetime.datetime.now()
+print(realDateTime.strftime("%c"))
 
-# for i in "Datacamp":
-#     if i == 'a':
-#         print (i)
+for i in "Datacamp":
+    if i == 'c':
+        print (i)
 
-# print("-----------------")
+print("-----------------")
 
-# for i in range(10):
-#     for j in range(i):
-#         print (i, end=' ')
-#     print()
+for i in range(10):
+    for j in range(i):
+        print (i, end=' ')
+    print()
 
 
-
+print("-----------------")
 
 # Python program to concatenate 
 # the result of two range functions 
